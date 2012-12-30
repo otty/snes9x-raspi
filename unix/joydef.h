d7e5745ea49a8f7c80391464bb288c88b9432368
adrian <aliasng@googlemail.com>
2012-12-30 17:37:50 +0100

    reassign gamepad buttons

--- unix/joydef.h
+++ unix/joydef.h
@@ -1,16 +1,16 @@
 // Defines for the joystick buttons. Use jstest(1) to figure these out.
 
 // Buttons
-#define JB_A 		4
-#define JB_B 		2
-#define JB_X 		5
+#define JB_A 		0
+#define JB_B 		1
+#define JB_X 		2
 #define JB_Y 		3
-#define JB_L 		6
-#define JB_R 		8
-#define JB_START 	11
-#define JB_SELECT 	10
-#define JB_ACCEL	9
-#define JB_QUIT		7
+#define JB_L 		4
+#define JB_R 		5
+#define JB_START 	9
+#define JB_SELECT 	8
+#define JB_ACCEL	10
+#define JB_QUIT		11
 
 // Axes
 #define JA_LR 0
@@ -20,5 +20,5 @@
 #define CENTER	0
 #define LEFT	1
 #define RIGHT	2
-#define UP	1
+#define UP	    1
 #define DOWN	2
